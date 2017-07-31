cc2099178ef9c744f679aa3f627365ec9dceccbb
Michael Delgado <mdelgado@rhg.com>
2017-07-31 11:34:13 -0700

    add jrnr.slurm_runner to module-level import

--- jrnr/__init__.py
+++ jrnr/__init__.py
@@ -2,6 +2,15 @@
 
 """Top-level package for jrnr."""
 
+from __future__ import absolute_import
+from jrnr.jrnr import slurm_runner
+
 __author__ = """Justin Simcock"""
 __email__ = 'jsimcock@rhg.com'
 __version__ = '0.1.1'
+
+_module_imports = (
+    slurm_runner,
+)
+
+__all__ = list(map(lambda x: x.__name__, _module_imports))
