843499630defcfb67afd1d268ab99629aa370522
Michael Delgado <mdelgado@rhg.com>
2017-07-31 17:00:54 -0700

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='jrnr',
-    version='0.2.0',
+    version='0.2.1',
     description="Job Runner for Climate Impact Lab Jobs",
     long_description=readme + '\n\n' + history,
     author="Justin Simcock",
