bbb9d6544126659d48e8e3dd1cf4bdcb319812b2
Michael Delgado <mdelgado@rhg.com>
2017-08-28 17:49:53 -0700

    Bump version: 0.2.1 → 0.2.2

--- jrnr/__init__.py
+++ jrnr/__init__.py
@@ -7,7 +7,7 @@ from jrnr.jrnr import slurm_runner
 
 __author__ = """Justin Simcock"""
 __email__ = 'jsimcock@rhg.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 _module_imports = (
     slurm_runner,
